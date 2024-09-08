d9e7794be57f6e9ea9f55ef4dcef3a2ce5e74a28
Adole Samuel Sani <21035760+adolesamuel@users.noreply.github.com>
2024-09-08 20:44:20 +0100

    Create longest_substring.dart

---
+++ src/longest_substring/longest_substring.dart
@@ -0,0 +1,19 @@
+import 'dart:math';
+
+/// Return the length of the longest substring.
+int longestSubstring(String s) {
+  final List<String> result = [];
+
+  int answer = 0;
+
+  for (int l = 0, r = 0; r < s.length; ++r) {
+    while (result.contains(s[r])) {
+      result.remove(s[l]);
+      l++;
+    }
+    result.add(s[r]);
+    answer = max(answer, r - l + 1);
+  }
+
+  return answer;
+}
