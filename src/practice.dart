8cadf02b44c185065ae68cd0d46dcb17822c5e0b
Adole Samuel Sani <adolesamuel4@gmail.com>
2025-01-10 15:48:32 +0100

    string atoi solution in Dart

--- src/practice.dart
+++ src/practice.dart
@@ -1,18 +1,38 @@
 import 'dart:math';
 
-int longestSubstring(String s) {
-  final List<String> result = [];
+List<int> mergeSort(List<int> list) {
+  final mid = list.length ~/ 2;
 
-  int answer = 0;
+  final left = mergeSort(list.sublist(0, mid));
+  final right = mergeSort(list.sublist(mid));
 
-  for (int l = 0, r = 0; r < s.length; r++) {
-    while (result.contains(s[r])) {
-      result.remove(s[l]);
-      l++;
+  return merge(left, right);
+}
+
+List<int> merge(List<int> left, List<int> right) {
+  List<int> result = [];
+  int i = 0;
+  int j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
+      result.add(left[i]);
+      i++;
+    } else {
+      result.add(right[j]);
+      j++;
     }
-    result.add(s[r]);
-    answer = max(answer, r - l + 1);
   }
 
-  return answer;
+  while (i < left.length) {
+    result.add(left[i]);
+    i++;
+  }
+
+  while (j < right.length) {
+    result.add(right[j]);
+    j++;
+  }
+
+  return result;
 }
--- src/practice.go
+++ src/practice.go
@@ -1,1 +1,44 @@
 package practice
+
+func mergeSort(list []int) []int {
+	if len(list) <= 1 {
+		return list
+	}
+	mid := len(list) / 2
+
+	left := mergeSort(list[0:mid])
+
+	right := mergeSort(list[mid:])
+
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	result := []int{}
+
+	i := 0
+	j := 0
+
+	for i < len(left) && j < len(right) {
+		if left[i] <= right[j] {
+			result = append(result, left[i])
+			i++
+		} else {
+			result = append(result, right[j])
+			j++
+		}
+
+	}
+
+	for i < len(left) {
+		result = append(result, left[i])
+		i++
+	}
+
+	for j < len(right) {
+		result = append(result, right[j])
+		j++
+	}
+
+	return result
+}
