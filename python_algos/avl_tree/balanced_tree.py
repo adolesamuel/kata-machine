eb54b498eaa71c14e0a2dd1ac7aafd3e5878faf5
Adole Samuel Sani <adolesamuel4@gmail.com>
2025-04-21 09:12:06 +0100

    avl tree

---
+++ python_algos/avl_tree/balanced_tree.py
@@ -0,0 +1,31 @@
+
+from python_algos.avl_tree.node import Node
+
+
+class BalancedTree(object):
+    def __init__(self):
+        self.rootNode = None
+
+    def insert(self,data):
+        parentNode = self.rootNode
+
+        if self.rootNode == None:
+            parentNode = Node(data,None)
+            self.rootNode = parentNode
+        else:
+            parentNode = self.rootNode.insert(data,self.rootNode)
+        self.rebalanceTree(parentNode)
+
+
+    def rebalanceTree(self,parentNode):
+        self.setBalance(parentNode)
+    
+    def setBalance(self, node):
+        node.balance = (self.height(node.rightChild) - self.height(node.leftChild))
+    
+    def height(self,node):
+        if node == None:
+            return -1
+        else:
+            return 1 + max(self.height(node.leftChild),self.height(node.rightChild))
+        
