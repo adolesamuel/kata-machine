55252b78811c9e03801f75390343a4376fc0894a
Adole Samuel Sani <adolesamuel4@gmail.com>
2025-05-01 16:21:40 +0100

    Update balanced_tree.py

--- python_algos/avl_tree/balanced_tree.py
+++ python_algos/avl_tree/balanced_tree.py
@@ -19,6 +19,49 @@ class BalancedTree(object):
 
     def rebalanceTree(self,parentNode):
         self.setBalance(parentNode)
+        if parentNode.balance < -1:
+            if self.height(parentNode.leftChild.leftChild) >= self.height(parentNode.leftChild.rightChild):
+                parentNode = self.rotateRight(parentNode)
+            else:
+                parentNode = self.rotateLeftRight(parentNode)
+        elif parentNode.balance > 1:
+            if self.height(parentNode.rightChild.rightChild) >= self.height(parentNode.rightChild.leftChild):
+                parentNode = self.rotateLeft(parentNode)
+            else:
+                parentNode = self.rotateRightLeft(parentNode)
+        if parentNode.parentNode != None:
+            self.rebalanceTree(parentNode.parentNode)
+        else:
+            self.rootNode = parentNode
+
+    def rotateLeftRight(self,node):
+        print("Rotation Left Right....")
+        node.leftChild = self.rotateLeft(node.leftChild)
+        return self.rotateRight(node)
+    
+    def rotateRightLeft(self,node):
+        print("Rotation Right Left....")
+        node.rightChild = self.rotateRight(node.rightChild)
+        return self.rotateLeft(node)
+    
+    def rotateLeft(self,node):
+        print("Rotation Left....")
+        newParent = node.rightChild
+        node.rightChild = newParent.leftChild
+        newParent.leftChild = node
+        self.setBalance(node)
+        self.setBalance(newParent)
+        return newParent
+    
+    def rotateRight(self,node):
+        print("Rotation Right....")
+        newParent = node.leftChild
+        node.leftChild = newParent.rightChild
+        newParent.rightChild = node
+        self.setBalance(node)
+        self.setBalance(newParent)
+        return newParent
+
     
     def setBalance(self, node):
         node.balance = (self.height(node.rightChild) - self.height(node.leftChild))
