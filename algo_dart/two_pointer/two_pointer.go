145c2b03d7b50801ee9a72a9bb268e80acbc0ef2
Adole Samuel Sani <adolesamuel4@gmail.com>
2026-02-01 18:32:48 +0100

    Update two_pointer.go

--- algo_dart/two_pointer/two_pointer.go
+++ algo_dart/two_pointer/two_pointer.go
@@ -125,3 +125,37 @@ func largestContainerArea(height []int) int {
 	return maxArea
 
 }
+
+func sortedSquares(nums []int) []int {
+	n := len(nums)
+	result := make([]int, n)
+
+	left, right := 0, n-1
+	pos := n - 1
+
+	for left <= right {
+		leftSq := nums[left] * nums[left]
+		rightSq := nums[right] * nums[right]
+
+		if leftSq > rightSq {
+			result[pos] = leftSq
+			left++
+		} else {
+			result[pos] = rightSq
+			right--
+		}
+		pos--
+	}
+
+	return result
+}
+
+func sortedSquares2(nums []int) []int {
+	n := len(nums)
+	result := make([]int, n)
+	left, right := 0, n-1
+
+	for left < right {
+		
+	}
+}
