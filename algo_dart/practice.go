546bfd2f6a696ea2cc676167c9dcdf124081c651
Adole Samuel Sani <adolesamuel4@gmail.com>
2026-05-12 09:20:44 +0100

    practice

--- algo_dart/practice.go
+++ algo_dart/practice.go
@@ -1,1 +1,30 @@
 package main
+
+// TreeNode Struct
+type TreeNode[E any] struct {
+	Value    E
+	Children []TreeNode[E]
+}
+
+// Add
+func (t *TreeNode[E]) Add(child TreeNode[E]) {
+	t.Children = append(t.Children, child)
+}
+
+//DFS
+
+func (t *TreeNode[E]) dfs(performAction func(node *TreeNode[E])) {
+	performAction(t)
+	for _, val := range t.Children {
+		val.dfs(performAction)
+	}
+}
+
+//BFS
+
+func (t *TreeNode[E]) bfs(performAction func(node *TreeNode[E])) {
+	for _, val := range t.Children {
+		val.bfs(performAction)
+	}
+	performAction(t)
+}
